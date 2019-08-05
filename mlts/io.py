0632c7523293e0b189c69bc2832a1fa06938de35
Andrei Nesterov <ae.nesterov@gmail.com>
2019-08-04 21:21:14 -0700

    Add `io.slice` function

--- mlts/io.py
+++ mlts/io.py
@@ -5,13 +5,13 @@ from functools import reduce
 import math
 
 def merge(data_acc):
-    """Merge multiple datasets into one."""
+    """Merge multiple dataframes into one."""
 
-    assert len(data_acc) > 0, "at least one dataset is provided"
+    assert len(data_acc) > 0, "at least one dataframe is provided"
     return reduce(lambda acc, data: acc.merge(data), data_acc[1:], data_acc[0])
 
 def check(data):
-    """Check dataset for undefined values."""
+    """Check the dataframe for undefined values."""
 
     assert type(data) == pd.core.frame.DataFrame
     assert sum(data.isna().sum()) == 0, "all elements of the dataset are defined"
@@ -32,8 +32,10 @@ def split(data, ycol, shape, seed = 1):
 
     # Separate labels from features
     n_x = len(data.columns) - 1
-    X = data.drop(data.columns[ycol], axis = 1).to_numpy(dtype=float).reshape((-1, n_x))
-    y = data.iloc[:, ycol].to_numpy(dtype=float).reshape((-1, 1))
+    ds = (
+        data.drop(data.columns[ycol], axis = 1).to_numpy(dtype=float).reshape((-1, n_x)),
+        data.iloc[:, ycol].to_numpy(dtype=float).reshape((-1, 1)),
+    )
 
     # Split the dataset into training, development, and testing
     m_train, m_dev, m_test = shape
@@ -42,13 +44,18 @@ def split(data, ycol, shape, seed = 1):
     idx_test = idx_dev + m_test
 
     return (
-        (X[0:idx_train, :], y[0:idx_train, :]),
-        (X[idx_train:idx_dev, :], y[idx_train:idx_dev, :]),
-        (X[idx_dev:idx_test, :], y[idx_dev:idx_test, :])
+        slice(ds, 0, idx_train),
+        slice(ds, idx_train, idx_dev),
+        slice(ds, idx_dev, idx_test),
     )
 
+def slice(ds, idx_start, idx_stop):
+    """Retrieve a slice of the dataset"""
+
+    return (ds[0][idx_start:idx_stop, :], ds[1][idx_start:idx_stop, :])
+
 def normalize(ds):
-    """Estimate normalization parametes on training dataset and apply them on development and testing datasets."""
+    """Estimate normalization parametes on the training dataset and apply them to development and testing datasets."""
 
     assert len(ds) == 3
     (X_train_in, y_train), (X_dev_in, y_dev), (X_test_in, y_test) = ds
